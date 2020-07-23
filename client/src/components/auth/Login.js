10c9d03523b8f83729b4809295feb1339f1a48d8
Mark Capili <angelocapili44@gmail.com>
2020-07-23 20:31:23 +0800

    login register css

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import AuthContext from '../../context/auth/authContext';
+import { Link } from 'react-router-dom';
 
 const Login = (props) => {
   const authContext = useContext(AuthContext);
@@ -36,39 +37,56 @@ const Login = (props) => {
   };
 
   return (
-    <div className='form-container'>
-      <h1>
-        Account <span className='text-primary'>Login</span>
-      </h1>
-      <form onSubmit={onSubmit}>
-        <div className='form-group'>
-          <label htmlFor='email'>Email Address</label>
-          <input
-            id='email'
-            type='email'
-            name='email'
-            value={email}
-            onChange={onChange}
-            required
-          />
+    <div className='auth'>
+      <div className='banner-container'>
+        <div className='brand-container'>
+          <i className='fas fa-bug main-icon' />
+          <div className='brand-name'>BUG TRACKER</div>
         </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password</label>
-          <input
-            id='password'
-            type='password'
-            name='password'
-            value={password}
-            onChange={onChange}
-            required
-          />
+
+        <div className='quote-container'>
+          <div className='quote'>
+            It's not a bug — it's an undocumented feature.
+          </div>
+          <div className='quote-author'>- Anonymous</div>
         </div>
-        <input
-          type='submit'
-          value='Login'
-          className='btn btn-primary btn-block'
-        />
-      </form>
+      </div>
+
+      <div className='login-form-container'>
+        <div className='auth-title'>LOGIN</div>
+        <div className='auth-sub'>Enter your login details below</div>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='email'>Email:</label>
+            <input
+              id='email'
+              type='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password:</label>
+            <input
+              id='password'
+              type='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <input type='submit' value='Login' className='auth-submit' />
+          <div className='register-prompt'>
+            Don't have an account?
+            <div>
+              <Link to='/register'>Sign up!</Link>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = (props) => {
   const [user, setUser] = useState({
@@ -17,63 +18,80 @@ const Register = (props) => {
   };
 
   return (
-    <div className='form-container'>
-      <h1>
-        Account <span className='text-primary'>Register</span>
-      </h1>
-      <form onSubmit={onSubmit}>
-        <div className='form-group'>
-          <label htmlFor='name'>Name</label>
-          <input
-            id='name'
-            type='text'
-            name='name'
-            value={name}
-            onChange={onChange}
-            required
-          />
+    <div className='auth'>
+      <div className='banner-container'>
+        <div className='brand-container'>
+          <i className='fas fa-bug main-icon' />
+          <div className='brand-name'>BUG TRACKER</div>
         </div>
-        <div className='form-group'>
-          <label htmlFor='email'>Email Address</label>
-          <input
-            id='email'
-            type='email'
-            name='email'
-            value={email}
-            onChange={onChange}
-            required
-          />
+
+        <div className='quote-container'>
+          <div className='quote'>
+            It's not a bug — it's an undocumented feature.
+          </div>
+          <div className='quote-author'>- Anonymous</div>
         </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password</label>
-          <input
-            id='password'
-            type='password'
-            name='password'
-            value={password}
-            onChange={onChange}
-            required
-            minLength='6'
-          />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='password2'>Confirm Password</label>
-          <input
-            id='password2'
-            type='password'
-            name='password2'
-            value={password2}
-            onChange={onChange}
-            required
-            minLength='6'
-          />
-        </div>
-        <input
-          type='submit'
-          value='Register'
-          className='btn btn-primary btn-block'
-        />
-      </form>
+      </div>
+
+      <div className='register-form-container'>
+        <div className='auth-title'>Register</div>
+        <div className='auth-sub'>Enter your details below</div>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='name'>Name</label>
+            <input
+              id='name'
+              type='text'
+              name='name'
+              value={name}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='email'>Email Address</label>
+            <input
+              id='email'
+              type='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              id='password'
+              type='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              required
+              minLength='6'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password2'>Confirm Password</label>
+            <input
+              id='password2'
+              type='password'
+              name='password2'
+              value={password2}
+              onChange={onChange}
+              required
+              minLength='6'
+            />
+          </div>
+          <input type='submit' value='Register' className='auth-submit' />
+          <div className='register-prompt'>
+            Already have an account?
+            <div>
+              <Link to='/login'>Login!</Link>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
